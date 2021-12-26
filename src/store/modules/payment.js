3b7b96da679781d95bbd25bfbcb8c4a0004ccbe2
Nguyễn Quang Duy <Quang Duy>
2021-12-27 00:27:34 +0700

    Edit payment

--- src/store/modules/payment.js
+++ src/store/modules/payment.js
@@ -17,7 +17,16 @@ const payment = {
             try{
                 const response = await axios.get('https://localhost:44337/api/payment');
                 console.log(response.data);
-                commit('SET_PAYMENT', response.data)
+                commit('SET_PAYMENT', response.data);
+            }catch(error){
+                console.log(error)
+            }
+        },
+        async addPayment({commit}){
+            try{
+                const response = await axios.get('https://localhost:44337/api/payment/add');
+                console.log(response.data);
+                commit('SET_PAYMENT', response.data);
             }catch(error){
                 console.log(error)
             }
