b7d8a021b72c550c31db764ce2f07c83b48c1b57
Nguyễn Quang Duy <nqduy98999@gmail.com>
2022-01-23 23:09:36 +0700

    Edit Shipper Order

--- src/component/shipper/order.vue
+++ src/component/shipper/order.vue
@@ -4,10 +4,10 @@
       <th class="col-md-2" style="font-weight: 400">{{order.fullname}}</th>
       <th class="col-md-2" style="font-weight: 400">{{order.address.home_number}} {{order.address.street}}, {{order.address.ward}}, {{order.address.district}}, {{order.address.province}}</th>
       <th class="col-md-1" style="font-weight: 400">{{order.phoneNumber}}</th>
-      <!-- <th class="col-md-2" style="font-weight: 400; padding: 0px;">
+      <th class="col-md-2" style="font-weight: 400; padding: 0px;">
           <span v-bind:class="classObject" style="font-size: 12px; margin-top: 15px;">{{order.status}}</span>
-      </th> -->
-      <th class="col-md-1" style="font-weight: 400">{{order.shipperId}}</th>
+      </th>
+      <!-- <th class="col-md-1" style="font-weight: 400">{{order.shipperId}}</th> -->
       <th class="col-md-2" style="font-weight: 400">{{order.shipperName}}</th>
       <th class="col-md-2" style="font-weight: 400">{{order.totalPrice}}</th>
       <th class="col-md-2" style="font-weight: 400"><router-link class="btn btn-outline-success" v-bind:to="'/shipper/receiveOrder/'+order.id">Nhận đơn</router-link></th>
@@ -34,10 +34,10 @@ export default {
                 btn:true, 
                 disabled: true, 
                 'btn-danger': this.order.status === 'Đã hủy',
-                'btn-primary': this.order.status === 'Tiếp nhận',
+                'btn-primary': this.order.status === 'Đã tiếp nhận',
                 'btn-success': this.order.status === 'Thành công',
                 'btn-info': this.order.status === 'Đang giao',
-                'btn-warning': this.order.status === 'Chuẩn bị',
+                'btn-warning': this.order.status === 'Chờ tiếp nhận',
             }
         }
     }
