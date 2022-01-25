79e3ebd5d867b7c853c7c361fba4a701ce76a6fd
Nguyễn Quang Duy <nqduy98999@gmail.com>
2022-01-25 18:32:28 +0700

    Add role Admin and Admin Header

--- src/component/user/login.vue
+++ src/component/user/login.vue
@@ -92,7 +92,7 @@ export default {
             }else if(role === 3){ // shipper
               this.$router.push('/shipper/order');
             }else{//admin
-
+              this.$router.push('/admin/order');
             }
             
           }
